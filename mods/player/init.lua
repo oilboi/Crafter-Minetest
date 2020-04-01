be9df592e049f24b5477f863799bf757d9964846
oilboi <47129783+oilboi@users.noreply.github.com>
2020-04-01 01:26:55 -0400

    Add in scary cave noises

--- mods/player/init.lua
+++ mods/player/init.lua
@@ -257,3 +257,26 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 		})
 	end
 end)
+
+
+--this is to check if the player is exploring a cave
+--if exploring cave not near an open shaft then play a scary noise
+--every 5-7 minutes
+local scary_sound_player_timer = 0
+minetest.register_globalstep(function(dtime)
+	scary_sound_player_timer = scary_sound_player_timer + dtime
+	--print(scary_sound_player_timer)
+	--try to play every 5 minutes
+	if scary_sound_player_timer > 300 then
+		scary_sound_player_timer = math.random(-120,0)
+		for _,player in ipairs(minetest.get_connected_players()) do
+			local pos = player:get_pos()
+			pos.y = pos.y + 1.625
+			local light = minetest.get_node_light(pos)
+			if pos.y < 0 and light <= 13 then
+				--print(light)
+				minetest.sound_play("scary_noise",{to_player = player:get_player_name(),gain=0.7,pitch=math.random(70,100)/100})
+			end	
+		end
+	end
+end)
