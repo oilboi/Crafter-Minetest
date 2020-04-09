c483da30564ecc79cb4005098d0f0212b76ee94e
oilboi <47129783+oilboi@users.noreply.github.com>
2020-04-08 23:36:53 -0400

    overhaul eating

--- mods/player/init.lua
+++ mods/player/init.lua
@@ -110,63 +110,73 @@ minetest.register_globalstep(function(dtime)
 	end
 end)
 
+--eating
 minetest.register_globalstep(function(dtime)
-	--collection
 	for _,player in ipairs(minetest.get_connected_players()) do
 		if player:get_player_control().RMB then
 			local health = player:get_wielded_item():get_definition().health
 			if health then
 				local meta = player:get_meta()
 				local eating = meta:get_float("eating")
+				local eating_timer = meta:get_float("eating_timer")
 				
-				if meta:get_int("eating_ps") == 0 then
-					local ps = minetest.add_particlespawner({
-						amount = 100,
-						time = 0,
-						minpos = {x=-0.2, y=-1.5, z=0.5},
-						maxpos = {x=0.2, y=1.7, z=0.5},
-						minvel = vector.new(-0.5,0,-0.5),
-						maxvel = vector.new(0.5,0,0.5),
-						minacc = {x=0, y=-9.81, z=1},
-						maxacc = {x=0, y=-9.81, z=1},
-						minexptime = 0.5,
-						maxexptime = 1.5,
-						minsize = 1,
-						maxsize = 2,
-						attached = player,
-						collisiondetection = true,
-						vertical = false,
-						texture = "treecapitator.png"
+				eating = eating + dtime
+				eating_timer = eating_timer + dtime
+				
+				local ps = minetest.add_particlespawner({
+					amount = 30,
+					time = 0.00001,
+					minpos = {x=-0.2, y=-1.5, z=0.5},
+					maxpos = {x=0.2, y=1.7, z=0.5},
+					minvel = vector.new(-0.5,0,-0.5),
+					maxvel = vector.new(0.5,0,0.5),
+					minacc = {x=0, y=-9.81, z=1},
+					maxacc = {x=0, y=-9.81, z=1},
+					minexptime = 0.5,
+					maxexptime = 1.5,
+					minsize = 0.5,
+					maxsize = 1,
+					attached = player,
+					collisiondetection = true,
+					collision_removal = true,
+					vertical = false,
+					texture = "eat_particles_1.png"
+				})
+				
+				if eating_timer + dtime > 0.25 then
+					minetest.sound_play("eat", {
+						to_player = player:get_player_name(),
+						gain = 1.0,  -- default
+						pitch = math.random(60,100)/100,
 					})
-					meta:set_int("eating_ps", ps)
+					eating_timer = 0
 				end
-					
-				if eating + dtime >= 2 then
+				
+				if eating + dtime >= 1 then
 					local stack = player:get_wielded_item()
 					stack:take_item(1)
 					player:set_wielded_item(stack)
 					player:set_hp(player:get_hp() + health)
 					eating = 0
-					minetest.sound_play("eat", {
-						object = player,
-						gain = 1.0,  -- default
-						max_hear_distance = 32,  -- default, uses an euclidean metric
-						pitch = math.random(70,100)/100,
+					minetest.sound_play("eat_finish", {
+						to_player = player:get_player_name(),
+						gain = 0.2,  -- default
+						pitch = math.random(60,85)/100,
 					})
 				end
-				meta:set_float("eating", eating + dtime)
+				
+				meta:set_float("eating_timer", eating_timer)
+				meta:set_float("eating", eating)
 			else
 				local meta = player:get_meta()
 				meta:set_float("eating", 0)
-				minetest.delete_particlespawner(meta:get_int("eating_ps"))
-				meta:set_int("eating_ps", 0)
+				meta:set_float("eating_timer", 0)
 				
 			end
 		else
 			local meta = player:get_meta()
 			meta:set_float("eating", 0)
-			minetest.delete_particlespawner(meta:get_int("eating_ps"))
-			meta:set_int("eating_ps", 0)
+			meta:set_float("eating_timer", 0)
 		end
 		
 	end
