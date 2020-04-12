c2757d61dc955774837685d9e54ce3a979465e79
oilboi <47129783+oilboi@users.noreply.github.com>
2020-04-12 19:35:18 -0400

    Move player effects clientside

--- mods/player/init.lua
+++ mods/player/init.lua
@@ -1,5 +1,4 @@
 local path = minetest.get_modpath("player")
 dofile(path.."/version_info.lua")
 dofile(path.."/player_interaction.lua")
-dofile(path.."/player_effects.lua")
 dofile(path.."/player_mechanics.lua")
