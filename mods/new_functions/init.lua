812c89f863e3a9568d5992ac60314ee794d4d2b1
oilboi <oil.boi.minetest@gmail.com>
2020-04-24 06:39:25 -0400

    Add in extremely efficient touch based damage

--- mods/new_functions/init.lua
+++ mods/new_functions/init.lua
@@ -1,4 +1,6 @@
 local get_group = minetest.get_node_group
+local registered_nodes
+local get_node = minetest.get_node
 --add nodes that hurt the player into the touch hurt table
 local hurt_nodes = {}
 minetest.register_on_mods_loaded(function()
@@ -7,6 +9,7 @@ minetest.register_on_mods_loaded(function()
 			table.insert(hurt_nodes,def.name)
 		end
 	end
+	registered_nodes = minetest.registered_nodes
 end)
 
 --handle nodes around, inside, and above
@@ -30,6 +33,8 @@ local get_node = minetest.get_node
 local pos
 local node
 local name
+local damage_pos
+local collisionbox
 local function index_players_surroundings()
 	for _,player in ipairs(minetest.get_connected_players()) do
 		if player:get_hp() > 0 then
@@ -51,12 +56,22 @@ local function index_players_surroundings()
 			player_surroundings_index_table[name].head = get_node(pos).name
 			
 			--used for finding a damage node next to the player (centered at player's waist)
-			pos.y = pos.y - 0.44
-			player_surroundings_index_table[name].hurt = minetest.find_node_near(pos, 1, hurt_nodes)
+			pos.y = pos.y - 0.74
+			damage_pos = minetest.find_node_near(pos, 1, hurt_nodes)
+			if damage_pos then
+				collisionbox = registered_nodes[get_node(damage_pos).name].collision_box
+				if not collisionbox then
+					collisionbox = {-0.5,-0.5,-0.5,0.5,0.5,0.5}
+				end
+				player_surroundings_index_table[name].hurt = {pos=damage_pos,collisionbox=collisionbox}
+			else
+				collisionbox = nil
+				player_surroundings_index_table[name].hurt = nil
+			end
 				
 		end
 	end
-	--10 times a second server tick
+	--4 times a second server tick
 	minetest.after(0.25, function()
 		index_players_surroundings()
 	end)
@@ -93,3 +108,72 @@ handle_drowning()
 ]]--
 
 --handle touching hurt
+local player_pos
+
+local c_pos
+
+local temp_hurt
+local x1
+local y1
+local z1
+
+local xcompare
+local ycompare
+local zcompare
+
+local c_player
+
+local subval = vector.subtract
+
+local abs_it = math.abs
+local floor_it = math.floor
+
+local heart
+
+local function handle_hurt()
+	c_player = minetest.get_connected_players()
+	for _,player in ipairs(c_player) do
+		if player:get_hp() > 0 then
+			player_pos = player:get_pos()
+			name = player:get_player_name()
+			temp_hurt = player_surroundings_index_table[name].hurt
+			if temp_hurt then
+				c_pos = temp_hurt.pos				
+				x1 = floor_it(abs_it(player_pos.x-c_pos.x)*100)
+				y1 = floor_it(abs_it(player_pos.y-c_pos.y)*100)
+				z1 = floor_it(abs_it(player_pos.z-c_pos.z)*100)
+				--we will assume the player cbox is equal as x=0.8,y=0.5,z=0.8
+				if x1 <= 80 and z1 <= 80 and y1 <= 50 then
+					print(x1,y1,z1)
+					heart = player:get_hp()
+					player:set_hp(heart - 1)
+				end
+			end
+		end
+	end
+	minetest.after(0.5, function()
+		handle_hurt()
+	end)
+end
+
+handle_hurt()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
