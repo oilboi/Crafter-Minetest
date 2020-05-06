78dbab851185048f670041becb7a58e1eb24908f
oilboi <47129783+oilboi@users.noreply.github.com>
2020-05-05 21:06:38 -0400

    Use actual state modifiers with ticks to control satiation and hunger instead of randomness

--- mods/hunger/init.lua
+++ mods/hunger/init.lua
@@ -28,6 +28,7 @@ minetest.register_on_newplayer(function(player)
 	if meta:get_int("hunger") == 0 then
 		meta:set_int("hunger", 20)
 		meta:set_int("satiation", 5)
+		meta:set_int("exhaustion_tick", 0)
 	end
 	
 end)
@@ -36,63 +37,142 @@ minetest.register_on_respawnplayer(function(player)
 	local meta = player:get_meta()
 	meta:set_int("hunger", 20)
 	meta:set_int("satiation", 5)
+	meta:set_int("exhaustion_tick", 0)
+	meta:set_int("dead", 0)
+	local hunger_bar = meta:get_int("hunger_bar")
+	player:hud_change(hunger_bar, "number", 20)
 end)
+
+minetest.register_on_dieplayer(function(player)
+	local meta = player:get_meta()
+	meta:set_int("dead", 1)
+end)
+
+--this is the max exhaustion a player will get before their
+--satiation goes down and rolls over
+local exhaustion_peak = 384
+--when satiation runs out this is when the hunger peak variable
+--is used, everytime the player rolls over this their hunger ticks down
+--based on what they're doing
+local hunger_peak = 64
+
 
 local function hunger_update()
 	for _,player in ipairs(minetest.get_connected_players()) do
+	
+		--get the metas
 		local meta = player:get_meta()
-		local satiation = meta:get_int("satiation")
-		local hunger = meta:get_int("hunger")
-		local running = (meta:get_string("player.player_movement_state") == "1")
-		local bunny_hopping = (meta:get_string("player.player_movement_state") == "2")
-		local sneaking = (meta:get_string("player.player_movement_state") == "3")		
-		local got_hungry = math.random()
-		if satiation > 0 then
-			if running and got_hungry > 0.959 then
-				satiation = satiation - 1
-			elseif bunny_hopping and got_hungry > 0.941 then
-				satiation = satiation - 1
-			elseif sneaking and got_hungry > 0.9930 then
-				satiation = satiation - 1
-			elseif got_hungry > 0.97 then
-				satiation = satiation - 1
-			end
-		end
-		if satiation == 0 then
-			if hunger > 0 then
-				if running and got_hungry > 0.922 then
-					hunger = hunger - 1
-				elseif bunny_hopping and got_hungry > 0.917 then
-					hunger = hunger - 1
-				elseif sneaking and got_hungry > 0.941 then
-					hunger = hunger - 1
-				elseif got_hungry > 0.937 then
-					hunger = hunger - 1
-				end
-			end
-			meta:set_int("hunger", hunger)
-			if hunger <= 0 then
-				local hp =  player:get_hp()
-				if hp > 0 then
-					player:set_hp(hp-1)
-				end
-			end
-		end
 		
-		local hp = player:get_hp()
-		if hunger >= 20 and hp < 20 then
-			player:set_hp(hp+1)
-			satiation = satiation - 1
-			if satiation < 0 then
-				satiation = 0
+		--do not regen player's health if dead - this will be reused for 1up apples
+		if meta:get_int("dead") == 0 then --and meta:get_int("regeneration")
+		
+			--internal variables
+			local satiation = meta:get_int("satiation")
+			local hunger = meta:get_int("hunger")
+			local exhaustion_tick = meta:get_int("exhaustion_tick")
+			
+			--movement states
+			local movement_state =  meta:get_string("player.player_movement_state")
+			local running = (movement_state == "1")
+			local bunny_hopping = (movement_state == "2")
+			local sneaking = (movement_state == "3")
+			local standing = false
+			local walking = false
+			
+			--we must seperate these two values because I forgot to
+			--write in a seperate clientside state for walking/standing
+			if movement_state == "0" then
+				local input = player:get_player_control()
+				if input.jump or input.right or input.left or input.down or input.up then
+					walking = true
+				else
+					standing = true
+				end
 			end
+			
+			--we count up the exhaustion of the player moving around
+			--based on their states
+			if satiation > 0 then
+				if running then
+					exhaustion_tick = exhaustion_tick + 6
+				elseif bunny_hopping then
+					exhaustion_tick = exhaustion_tick + 8
+				elseif sneaking then
+					exhaustion_tick = exhaustion_tick + 1
+					
+				elseif walking then
+					exhaustion_tick = exhaustion_tick + 3
+				elseif standing then
+					exhaustion_tick = exhaustion_tick + 1
+				end
+				
+				
+				if exhaustion_tick >= exhaustion_peak then
+					print("should be wroking")
+					satiation = satiation - 1
+					exhaustion_tick = exhaustion_tick - exhaustion_peak
+					
+					--reset this to use for the hunger tick
+					if satiation == 0 then
+						exhaustion_tick = 0
+					end
+					
+					meta:set_int("satiation", satiation)
+				end
+				
+				meta:set_int("exhaustion_tick", exhaustion_tick)
+			elseif hunger > 0 then
+				--this is copied again because this is for future tuning
+				if running then
+					exhaustion_tick = exhaustion_tick + 3
+				elseif bunny_hopping then
+					exhaustion_tick = exhaustion_tick + 4
+				elseif sneaking then
+					exhaustion_tick = exhaustion_tick + 1
+				elseif walking then
+					exhaustion_tick = exhaustion_tick + 2
+				elseif standing then
+					exhaustion_tick = exhaustion_tick + 1
+				end
+				if exhaustion_tick >= hunger_peak then
+					--don't allow hunger to go negative
+					if hunger > 0 then
+						exhaustion_tick = 0
+						hunger = hunger - 1
+						meta:set_int("hunger", hunger)
+						local hunger_bar = meta:get_int("hunger_bar")
+						player:hud_change(hunger_bar, "number", hunger)
+					end
+				end
+				meta:set_int("exhaustion_tick", exhaustion_tick)
+			elseif hunger <= 0 then
+				exhaustion_tick = exhaustion_tick + 1
+				
+				local hp =  player:get_hp()
+				if hp > 0 and exhaustion_tick >= 2 then
+					player:set_hp(hp-1)
+					exhaustion_tick = 0
+				end
+				meta:set_int("exhaustion_tick", exhaustion_tick)
+			end
+			
+			print("satiation:",satiation,"exhaustion_tick:",exhaustion_tick)
+			local hp = player:get_hp()
+			if hunger >= 20 and hp < 20 then
+				player:set_hp(hp+1)
+				exhaustion_tick = 0
+				satiation = satiation - 1
+				if satiation < 0 then
+					satiation = 0
+				end
+				meta:set_int("exhaustion_tick", exhaustion_tick)
+			end
+			
+			meta:set_int("satiation", satiation)
 		end
-		meta:set_int("satiation", satiation)
-		local hunger_bar = meta:get_int("hunger_bar")
-		player:hud_change(hunger_bar, "number", hunger)
 	end
 	
-	minetest.after(1, function()
+	minetest.after(0.5, function()
 		hunger_update()
 	end)
 end
@@ -102,28 +182,10 @@ hunger_update()
 --take away hunger and satiation randomly while mining
 minetest.register_on_dignode(function(pos, oldnode, digger)
 	local meta = digger:get_meta()
-	local satiation = meta:get_int("satiation")
-	local hunger = meta:get_int("hunger")
-	local got_hungry = math.random()
-	if satiation > 0 then
-		if got_hungry > 0.955 then
-			satiation = satiation - 1
-			meta:set_int("satiation", satiation)
-		end
-	end
-	
-	if satiation == 0 then
-		if hunger > 0 then
-			if got_hungry > 0.925 then
-				hunger = hunger - 1
-				meta:set_int("hunger", hunger)
-				local hunger_bar = meta:get_int("hunger_bar")
-				digger:hud_change(hunger_bar, "number", hunger)
-			end
-		end
-	end
+	local exhaustion_tick = meta:get_int("exhaustion_tick")
+	exhaustion_tick = exhaustion_tick + math.random(0,2)
+	meta:set_int("exhaustion_tick", exhaustion_tick)
 end)
-
 
 --allow players to eat food
 function minetest.eat_food(player,item)
@@ -157,6 +219,7 @@ function minetest.eat_food(player,item)
 		end
 	end
 	
+	meta:set_int("exhaustion_tick", 0)
 	meta:set_int("hunger", player_hunger)
 	meta:set_int("satiation", player_satiation)
 	local hunger_bar = meta:get_int("hunger_bar")
