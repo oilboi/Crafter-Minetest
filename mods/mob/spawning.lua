4cccc17d92a7b8c1f1f7b10ee70a74c0926c1756
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-05 07:23:41 -0400

    Make spiders and creepers spawn more often at night

--- mods/mob/spawning.lua
+++ mods/mob/spawning.lua
@@ -15,7 +15,7 @@ local spawn = true
 
 local spawn_table = {"pig","chicken","sheep"}
 local snow_spawn_table = {"snowman","creeper","spider",}
-local dark_spawn_table = {"creeper","spider","big_slime","medium_slime","small_slime"}
+local dark_spawn_table = {"creeper","creeper","creeper","spider","spider","spider","big_slime","medium_slime","small_slime"}
 local nether_spawn_table = {"nitro_creeper"}
 local aether_spawn_table = {"phyg"}
 
