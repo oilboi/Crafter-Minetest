0c8110df108b9ba39b72c8f3ecaa67c275960f9a
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-17 18:06:10 -0400

    Add in creative mode hand

--- mods/hand/init.lua
+++ mods/hand/init.lua
@@ -3,8 +3,16 @@
 --Create an initial hand tool
 minetest.register_item(":", {
 	type = "none",
-	wield_image = "wieldhand.png",
+	wield_image = "nothing.png",
 	wield_scale = {x=1,y=1,z=2.5},
+})
+
+-- This is a fake node that should never be placed in the world
+local def = minetest.registered_items[""]
+minetest.register_node("hand:player", {
+	description = "",
+	tiles = {"player.png"},
+	visual_scale = 1,
 	tool_capabilities = {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
@@ -26,15 +34,48 @@ minetest.register_item(":", {
 			dig_immediate = {times={[2]=0,[3]=0,[1]=0,},uses=0,maxlevel=1},
 		},
 		damage_groups = {damage=1},
-	}
+	},
+	wield_scale = {x=1,y=1,z=1},
+	paramtype = "light",
+	drawtype = "mesh",
+	mesh = "hand.b3d",
+	-- Prevent construction
+	node_placement_prediction = "",
+	on_construct = function(pos)
+		minetest.log("error", "Tried to place hand at "..minetest.pos_to_string(pos))
+		minetest.remove_node(pos)
+	end,
+	drop = "",
+	on_drop = function()
+		return ""
+	end,
+	groups = { dig_immediate = 3, not_in_creative_inventory = 1 },
+	range = def.range,
 })
 
--- This is a fake node that should never be placed in the world
-local def = minetest.registered_items[""]
-minetest.register_node("hand:player", {
+minetest.register_node("hand:creative", {
 	description = "",
 	tiles = {"player.png"},
 	visual_scale = 1,
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {		
+			stone = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			dirt =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			snow =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			grass = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			sand =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			wood =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			leaves ={times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			wool =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			glass = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
+			netherrack = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0},uses=0, maxlevel=1},
+			unbreakable = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0},uses=0, maxlevel=1},
+			dig_immediate = {times={[2]=0,[3]=0,[1]=0,},uses=0, maxlevel=1},
+		},
+		damage_groups = {damage=1},
+	},
 	wield_scale = {x=1,y=1,z=1},
 	paramtype = "light",
 	drawtype = "mesh",
