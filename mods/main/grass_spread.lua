000e2be7cf9110c26478b14f19c7c298e8fa674a
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-18 14:58:20 -0400

    Make grass spread faster

--- mods/main/grass_spread.lua
+++ mods/main/grass_spread.lua
@@ -1,15 +1,16 @@
+local minetest = minetest
 --grass spread abm
+local light
 minetest.register_abm({
 	label = "Grass Grow",
 	nodenames = {"main:dirt"},
-	neighbors = {"main:grass"},
+	neighbors = {"main:grass", "air"},
 	interval = 10,
-	chance = 2000,
+	chance = 1000,
 	action = function(pos)
-		local light = minetest.get_node_light(pos, nil)
+		light = minetest.get_node_light(pos, nil)
 		--print(light)
 		if light < 10 then
-			--print("failed to grow grass at "..dump(pos))
 			return
 		end
 		minetest.set_node(pos,{name="main:grass"})
