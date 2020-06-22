334825ca868135ed53e1d8e95c946426aa8801fa
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-22 15:28:00 -0400

    make mob animations reuse data

--- mods/mob/api/animation.lua
+++ mods/mob/api/animation.lua
@@ -1,4 +1,10 @@
 local math,vector = math,vector
+
+local speed
+local self_rotation
+local currentvel
+local goal = vector.new(0,0,0)
+local acceleration
 -- 
 mobs.create_animation_functions = function(def,mob_register)
 	if def.movement_type ~= "jump" then
@@ -12,7 +18,7 @@ mobs.create_animation_functions = function(def,mob_register)
 					self.current_animation = 1
 				end
 				
-				local speed = self.object:get_velocity()
+				speed = self.object:get_velocity()
 				speed.y = 0
 				self.object:set_animation_frame_speed(vector.distance(vector.new(0,0,0),speed)*def.animation_multiplier)
 			end
@@ -24,7 +30,7 @@ mobs.create_animation_functions = function(def,mob_register)
 		if self.death_animation_timer >= 0 and self.dead == true then
 			self.death_animation_timer = self.death_animation_timer - dtime
 			
-			local self_rotation = self.object:get_rotation()
+			self_rotation = self.object:get_rotation()
 			
 			if self.death_rotation == "x" then
 				if self_rotation.x < math.pi/2 then
@@ -39,9 +45,8 @@ mobs.create_animation_functions = function(def,mob_register)
 			end
 			
 			--print(self.death_animation_timer)
-			local currentvel = self.object:get_velocity()
-			local goal = vector.new(0,0,0)
-			local acceleration = vector.new(goal.x-currentvel.x,0,goal.z-currentvel.z)
+			currentvel = self.object:get_velocity()
+			acceleration = vector.new(goal.x-currentvel.x,0,goal.z-currentvel.z)
 			acceleration = vector.multiply(acceleration, 0.05)
 			self.object:add_velocity(acceleration)
 			self.object:set_animation(def.standing_frame, 15, 0, true)
