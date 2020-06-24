18acda95d52494a94f1ab16900ea5aa5ad76251e
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-23 23:10:33 -0400

    Speed up player flow calc

--- mods/player_mechanics/flowing.lua
+++ mods/player_mechanics/flowing.lua
@@ -8,16 +8,22 @@ local pool = {}
 
 local legs
 local flowing
+local c_flow
+local vel
+local acceleration
+local newvel
+local flow_dir
+local name
 minetest.register_globalstep(function()
     for _,player in ipairs(minetest.get_connected_players()) do
-        local flow_dir = flow(player:get_pos())
-        local name = player:get_player_name()
+        flow_dir = flow(player:get_pos())
+        name = player:get_player_name()
         if flow_dir then
             --buffer continuation
             if pool[name] then
-                local c_flow = pool[name]
-                local vel = player:get_player_velocity()
-                local acceleration
+                c_flow = pool[name]
+                vel = player:get_player_velocity()
+                acceleration = nil
                 if c_flow.x ~= 0 then
                     acceleration = vector.new(c_flow.x,0,0)
                 elseif c_flow.z ~= 0 then
@@ -26,7 +32,7 @@ minetest.register_globalstep(function()
                 acceleration = vector.multiply(acceleration, 0.075)
                 player:add_player_velocity(acceleration)
 
-                local newvel = player:get_player_velocity()
+                newvel = player:get_player_velocity()
 
                 if newvel.x ~= 0 or newvel.z ~= 0 then
                     return
@@ -35,8 +41,8 @@ minetest.register_globalstep(function()
                 end
             else
                 flow_dir = vector.multiply(flow_dir,10)
-                local vel = player:get_player_velocity()
-                local acceleration
+                vel = player:get_player_velocity()
+                acceleration = nil
                 if flow_dir.x ~= 0 then
                     acceleration = vector.new(flow_dir.x,0,0)
                 elseif flow_dir.z ~= 0 then
