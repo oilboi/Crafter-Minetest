7687af0cd58c4e3764082c7d61712b896dba56fc
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-30 22:43:10 -0400

    Remove old code

--- mods/redstone/player_detector.lua
+++ mods/redstone/player_detector.lua
@@ -46,37 +46,3 @@ minetest.register_lbm({
 })
 
 end
-
---[[
-
-minetest.register_abm{
-    label = "Redstone Player Detection",
-	nodenames = {"group:redstone_player_detection"},
-	--neighbors = {"group:water"},
-	interval = 0.2,
-	chance = 1,
-	action = function(pos)
-		local found_player = false
-		for _,object in ipairs(minetest.get_objects_inside_radius(pos, 9)) do
-			if object:is_player() and object:get_hp() > 0 then
-				local level = minetest.get_item_group(minetest.get_node(pos).name, "redstone_power")
-				found_player = true
-				local pos2 = object:get_pos()
-				pos2 = vector.floor(vector.add(pos2,0.5))
-				local distance = math.floor(vector.distance(pos2,pos))
-				distance = math.abs(distance - 9)
-				--print(distance)
-				if level ~= distance then
-					minetest.set_node(pos,{name="redstone:player_detector_"..distance})
-					redstone.collect_info(pos)
-					--print(distance)
-				end
-			end
-		end
-		if found_player == false then
-			minetest.swap_node(pos,{name="redstone:player_detector_0"})
-			redstone.collect_info(pos)
-		end
-	end,
-}
-]]--
